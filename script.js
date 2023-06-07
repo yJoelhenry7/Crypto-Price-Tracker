df6e2e68f4d053c28b85554a535a9313a29015d1
yJoelhenry7 <20pa1a5458@vishnu.edu.in>
2023-06-07 11:33:50 +0530

    Adding Files

---
+++ script.js
@@ -0,0 +1,48 @@
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
+
+// Drag down Menu Buttons
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+//   Circular timer
+var timer= document.getElementById('timer');
+var time=59;
+setInterval(function(){
+  timer.innerHTML=time;
+  time--;
+  if(time<=0) time =60;
+},1000)
